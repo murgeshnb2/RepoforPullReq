4b37d9ee0df45dc9750bbfc711c6eae8d487c092
Dattu V Kulkarni <Dattu V Kulkarni@LAPTOP-P09V1KSC>
2022-01-26 21:22:13 +0530

    first comment from manager for pullreq ex

---
+++ src/test/java/organisation/CreateContact.java
@@ -0,0 +1,12 @@
+package organisation;
+
+public class CreateContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Dooosraa");
+
+	}
+
+}
